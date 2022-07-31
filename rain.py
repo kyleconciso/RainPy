3b860da549c9ed1c5cea77c14cf00ba09b6cb83a
kyleconciso <kyleconciso@gmail.com>
2022-08-01 03:09:42 +0800

    New splash, wind parameters.
    Added new splash effect for raindrops and a wind effect.

--- rain.py
+++ rain.py
@@ -1,4 +1,4 @@
-# This is a python program that displays animated rain
+# This is a python program that displays falling raindrops
 
 import time
 import random
@@ -7,17 +7,18 @@ import random
 # Config
 WIDTH = int(input("Enter width: "))
 HEIGHT = int(input("Enter height: "))
-DELAY = float(input("Enter delay: "))
+DELAY = float(input("Enter delay (Recommended -> 0.05): "))
 CHANCE_MULTIPLIER = float(input("Raindrop chance multiplier (Recommended -> 0.01): "))
+WIND = int(input("Enter wind direction (+-0): "))
 INVERSE = bool(input("Inverse? (True, False): "))
-RAIN = ['*']
+RAIN = ['|']
+SPLASH= ['-']
 EMPTY = " "
 
 # Initialize box
 box = []
 for y in range(HEIGHT):
     box.append([EMPTY for i in range(WIDTH)])
-
 
 
 # Funcs
@@ -48,11 +49,26 @@ def populate():
     box[len(box)-1] = [EMPTY for i in range(WIDTH)]
     
     # Randomizer
-
     for i, char in enumerate(box[len(box)-1]):
         if random.randint(0,100) <= 100*CHANCE_MULTIPLIER:
             box[len(box)-1][i] = random.choice(RAIN)
 
+    # Splash
+    for i, char in enumerate(box[0]):
+        if char in RAIN:
+            box[0][i] = random.choice(SPLASH)
+    
+    # Wind
+    for i, y in enumerate(box):
+        if WIND>0:
+            for _ in range(abs(WIND)):
+                box[i].insert(0, EMPTY)
+                del box[i][WIDTH]
+
+        elif WIND<0:
+            for _ in range(abs(WIND)):
+                box[i].append(EMPTY)
+                del box[i][0]
 
 
 # Loop
